ff38e37896ba5accaeae708f0e500ca434696551
Jess Izen <jlizen@amazon.com>
2026-04-18 19:25:55 -0700

    Regenerate

---
+++ benches/example_bench.rs
@@ -0,0 +1,17 @@
+use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
+use ci_battery_pack_test::add;
+
+fn bench_add(c: &mut Criterion) {
+    let mut group = c.benchmark_group("add");
+    for (left, right) in [(1, 2), (100, 200), (u64::MAX / 2, u64::MAX / 2)] {
+        group.bench_with_input(
+            BenchmarkId::new("add", format!("{left}+{right}")),
+            &(left, right),
+            |b, &(l, r)| b.iter(|| add(black_box(l), black_box(r))),
+        );
+    }
+    group.finish();
+}
+
+criterion_group!(benches, bench_add);
+criterion_main!(benches);
